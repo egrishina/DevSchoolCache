d4409cf0c4b8770782f81e92b432aed8780ce833
Yusupova Ramilya <yusupova@mindbox.cloud>
2024-06-28 16:46:31 +0300

    Cleanup

--- DevSchoolCache/IMemoryCacheAdapter.cs
+++
@@ -1,6 +0,0 @@
-namespace DevSchoolCache;
-
-public interface IMemoryCacheAdapter<T>
-{
-    public Task<T> GetOrAddAsync(string key);
-}
--- DevSchoolCacheTests/UnitTest1.cs
+++
@@ -1,9 +0,0 @@
-using DevSchoolCache;
-using Moq;
-
-namespace DevSchoolCacheTests;
-
-[TestClass]
-public class UnitTest1
-{
-}
